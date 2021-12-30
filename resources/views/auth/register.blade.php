265edb8401ba635f9e0ca237185abf21e31d2148
Feyyaz Can Köse <user.name>
2021-12-30 14:40:55 +0300

    Ogrenci Numarası Kayıt için eklendi

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -26,6 +26,20 @@
                         </div>
 
                         <div class="row mb-3">
+                            <label for="ogrenci" class="col-md-4 col-form-label text-md-end">{{ __('ogrenci Numarası') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="ogrenci" type="text" class="form-control @error('ogrenci') is-invalid @enderror" name="ogrenci" value="{{ old('ogrenci') }}" required autocomplete="ogrenci" autofocus>
+
+                                @error('ogrenci')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
                             <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
